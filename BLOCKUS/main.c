7ac3686b427be4f0c11a17b08d420e657b791305
Myeongjun <bluejun0901@naver.com>
2024-07-31 21:33:26 +0900

    Initial Commit

---
+++ BLOCKUS/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  BLOCKUS
+//
+//  Created by 김명준 on 7/31/24.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
