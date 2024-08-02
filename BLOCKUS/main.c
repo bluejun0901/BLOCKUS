5c225618e611490aeb917f53be96b3b3ab6336bc
Myeongjun <bluejun0901@naver.com>
2024-08-02 20:06:35 +0900

    코드 정리

--- BLOCKUS/main.c
+++ BLOCKUS/main.c
@@ -26,6 +26,12 @@ const int fps = 60;
 int screen_width = 1000; // 화면 가로 크기(px)
 int screen_height = 600; // 화면 세로 크기(px)
 
+// 카메라 설정
+// TODO: 카메라 방향 등 정의
+double camera_x = 0.0;
+double camera_y = 0.0;
+double camera_z = 5.0;
+
 // 블록 종류 정의
 typedef enum _block_type {
     EMPTY = 0,
@@ -43,18 +49,12 @@ typedef enum _block_type {
 #define max_height 100 // 게임판에 놓을 수 있는 블록의 최대 높이
 block_type board[max_height][board_height][board_width]; // board[z좌표][y좌표][x좌표]로 접근: 디버깅에 편리함
 
-// 카메라 설정
-// TODO: 카메라 방향 등 정의
-double camera_x = 0.0;
-double camera_y = 0.0;
-double camera_z = 5.0;
-
 // 큐브 모양 정의
 // cube_data[_][z좌표][y좌표][x좌표]로 접근
 // rotated_cube_data[_][z축 회전 * 16 + y축 회전 * 4 + x축 회전][z좌표][y좌표][x좌표]로 접근
-#define cube_size 4 // 하나의 큐브를 저장하는 데 사용되는 공간
+#define cube_size 4 // 하나의 큐브를 저장하는 데 사용되는 공간의 크기 = 가장 큰 큐브의 크기
 #define cube_num 11 // 큐브 종류의 수
-const int fit_cube_size[cube_num] = { 2, 3, 2, 4, 2, 3, 3, 3, 2, 2, 2 }; // 큐브가 차지하는 실제 공간
+const int fit_cube_size[cube_num] = { 2, 3, 2, 4, 2, 3, 3, 3, 2, 2, 2 }; // 큐브가 차지하는 실제 공간의 크기
 const block_type cube_data[cube_num][cube_size][cube_size][cube_size] = {
     { // cube2-0
         {
@@ -302,14 +302,197 @@ const block_type cube_data[cube_num][cube_size][cube_size][cube_size] = {
 };
 block_type rotated_cube_data[cube_num][4 * 4 * 4][cube_size][cube_size][cube_size]; // main함수에서 전처리
 
-// 큐브 정의
+// 큐브, 큐브 관련 함수 정의
 typedef struct _cube {
-    int cube_type;        // cube_data의 인덱스를 가리킴
+    int cube_type;        // 0 ~ cube_num - 1의 정수 값을 가지며 큐브의 모양을 가리킴
     int x_rotation;       // x축을 기준으로 회전한 횟수
     int y_rotation;       // y축을 기준으로 회전한 횟수
     int z_rotation;       // z축을 기준으로 회전한 횟수
     bool color_inversion; // 색 반전 여부
 } cube;
+/* 큐브를 받아 큐브의 최종 형태를 반환하는 함수 */
+/* a: 큐브 구조체 */
+/* return: 큐브의 형태를 나타내는 cube_size * cube_size * cube_size크기의 3차원 배열을 가리키는 포인터 */
+void* cube_getArray(cube a);
+/* 큐브를 특정 위치에 놓을 수 있는지 여뷰를 반환하는 함수 */
+/* a: 큐브 구조체 */
+/* z0, y0, x0: 보드 좌표계에서 큐브의 왼쪽 아래 앞 꼭짓점의 위치 */
+/* return: 큐브를 특정 위치에 놓을 수 있는지 여부 */
+bool cube_canPut(cube a, int z0, int y0, int x0);
+/* 큐브를 특정 위치에 그리는 함수 */
+/* a: 큐브 구조체 */
+/* z0, y0, x0: 보드 좌표계에서 큐브의 왼쪽 아래 앞 꼭짓점의 위치 */
+void cube_draw(cube a, double z0, double y0, double x0);
+
+int test_variable = 0; // test code
+int tv2 = 0; // test code
+int tv3 = 0; // test code
+
+// 그리기 함수
+/* 단위 정육면체를 그리는 함수 */
+/* z, y, x: 보드 좌표계에서 정육면체의 왼쪽 아래 앞 꼭짓점의 위치 */
+/* type: 정육면체의 종류 */
+/* delta: 0.0 ~ 0.5의 실수값을 가지며, 블록의 색이 회색에 가까울 정도를 나타냄 */
+void drawBlock(double z, double y, double x, block_type type, double delta);
+
+/* 윈도우 크기가 변했을 때 실행될 함수 */
+void myReshape(int width, int height) {
+    // 투영 행렬 설정
+    glViewport(0, 0, width, height);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluPerspective(40.0, (double)width / (double)height, 0.1, 100.0);
+    
+    screen_width = width;
+    screen_height = height;
+    
+    glutPostRedisplay();
+}
+
+/* 배경을 렌더링할 때 사용할 함수 */
+void myDisplay(void) {
+    glClearColor(0.5, 0.5, 0.5, 1.0); // 배경색 지정
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    
+    // 카메라 설정
+    gluLookAt(-camera_x, camera_z, camera_y,               // 카메라 위치
+              -board_width / 2.0, 0.0, board_height / 2.0, // 바라볼 좌표: 보드의 가운데를 바라보도록 설정
+              0.0, 1.0, 0.0);                              // 위쪽을 가리키는 벡터
+    
+    // 게임판 그리기
+    for (int z = 0; z < max_height; z++) {
+        for (int y = 0; y < board_height; y++) {
+            for (int x = 0; x < board_width; x++) {
+                drawBlock(z, y, x, board[z][y][x], 0.0);
+            }
+        }
+    }
+    
+    // test code
+    cube t = { 8, 0, 0, 0, false };
+    cube_draw(t, test_variable, tv2, tv3);
+    // test code
+    
+    glutSwapBuffers();
+}
+
+/* 키보드 입력을 받는 함수 */
+void myKeyboard(unsigned char key, int x, int y) {
+    // wasd키로 카메라 위치 조절
+    // 임시 코드로 자연스럽이 않음
+    // 한글 키로 바뀌어 있으면 작동하지 않음
+    // TODO: 카메라가 자연스럽게 움직이도록 코드 변경
+    if (key == 'w' || key == 'W') {
+        camera_y += 1.0;
+    } else if (key == 's' || key == 'S') {
+        camera_y -= 1.0;
+    } else if (key == 'a' || key == 'A') {
+        camera_x -= 1.0;
+    } else if (key == 'd' || key == 'D') {
+        camera_x += 1.0;
+    }
+    // test code
+    else if (key == 'r' || key == 'R') {
+        test_variable++;
+    } else if (key == 't' || key == 'T') {
+        tv2++;
+    } else if (key == 'y' || key == 'Y') {
+        tv3++;
+    }
+    
+    glutPostRedisplay();
+}
+
+/* 특수키 입력을 받는 함수 */
+void mySpecialKey(int key, int x, int y) {
+    // 방향키로 카메라 위치 조절
+    // 임시 코드로 자연스럽이 않음
+    // TODO: 카메라가 자연스럽게 움직이도록 코드 변경
+    if (key == GLUT_KEY_UP) {
+        camera_y += 1.0;
+    } else if (key == GLUT_KEY_DOWN) {
+        camera_y -= 1.0;
+    } else if (key == GLUT_KEY_LEFT) {
+        camera_x -= 1.0;
+    } else if (key == GLUT_KEY_RIGHT) {
+        camera_x += 1.0;
+    }
+}
+
+int main(int argc, char * argv[]) {
+    // 보드 초기화
+    for (int z = 0; z < max_height; z++) {
+        for (int y = 0; y < board_height; y++) {
+            for (int x = 0; x < board_width; x++) {
+                if (z == 0) {
+                    board[z][y][x] = ((x + y) % 2 == 0 ? WHITE : BLACK);
+                } else {
+                    board[z][y][x] = EMPTY;
+                }
+            }
+        }
+    }
+    
+    // 회전된 큐브 데이터 초기화
+    for (int cube_type = 0; cube_type < cube_num; cube_type++) {
+        int fit_size = fit_cube_size[cube_type];
+        block_type array[cube_size][cube_size][cube_size];
+        memcpy(array, cube_data[cube_type], cube_size * cube_size * cube_size * sizeof(block_type));
+        
+        block_type temp[cube_size][cube_size][cube_size];
+        for (int x_rotation = 0; x_rotation < 4; x_rotation++) {
+            for (int y_rotation = 0; y_rotation < 4; y_rotation++) {
+                for (int z_rotation = 0; z_rotation < 4; z_rotation++) {
+                    memcpy(rotated_cube_data[cube_type][z_rotation * 16 + y_rotation * 4 + x_rotation], array, cube_size * cube_size * cube_size * sizeof(block_type));
+                    
+                    memcpy(temp, array, cube_size * cube_size * cube_size * sizeof(block_type));
+                    for (int x = 0; x < fit_size; x++) {
+                        for (int y = 0; y < fit_size; y++) {
+                            for (int z = 0; z < fit_size; z++) {
+                                array[z][y][x] = temp[z][fit_size - 1 - x][y];
+                            }
+                        }
+                    }
+                }
+                memcpy(temp, array, cube_size * cube_size * cube_size * sizeof(block_type));
+                for (int x = 0; x < fit_size; x++) {
+                    for (int y = 0; y < fit_size; y++) {
+                        for (int z = 0; z < fit_size; z++) {
+                            array[z][y][x] = temp[fit_size - 1 - x][y][z];
+                        }
+                    }
+                }
+            }
+            memcpy(temp, array, cube_size * cube_size * cube_size * sizeof(block_type));
+            for (int x = 0; x < fit_size; x++) {
+                for (int y = 0; y < fit_size; y++) {
+                    for (int z = 0; z < fit_size; z++) {
+                        array[z][y][x] = temp[fit_size - 1 - y][z][x];
+                    }
+                }
+            }
+        }
+    }
+    
+    // 윈도우 생성과 초기화
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+    glutCreateWindow("BLOCKUS");
+    glutReshapeWindow(screen_width, screen_height);
+    
+    glEnable(GL_DEPTH_TEST);
+    
+    // 콜백 설정
+    glutDisplayFunc(myDisplay);
+    glutReshapeFunc(myReshape);
+    glutKeyboardFunc(myKeyboard);
+    
+    glutMainLoop();
+    
+    return 0;
+}
 
 void* cube_getArray(cube a) {
     a.x_rotation %= 4;
@@ -406,7 +589,21 @@ bool cube_canPut(cube a, int z0, int y0, int x0) {
     return true;
 }
 
-void drawCube(double z, double y, double x, block_type type, double delta) {
+void cube_draw(cube a, double z0, double y0, double x0) {
+    block_type (*array)[cube_size][cube_size] = cube_getArray(a);
+    
+    bool abled = cube_canPut(a, z0, y0, x0);
+    
+    for (int z = 0; z < cube_size; z++) {
+        for (int y = 0; y < cube_size; y++) {
+            for (int x = 0; x < cube_size; x++) {
+                drawBlock(z0 + z, y0 + y, x0 + x, array[z][y][x], (abled ? 0.1 : 0.3));
+            }
+        }
+    }
+}
+
+void drawBlock(double z, double y, double x, block_type type, double delta) {
     glPushMatrix();
     glTranslated(-x, z, y);
     
@@ -423,177 +620,4 @@ void drawCube(double z, double y, double x, block_type type, double delta) {
     
     glutSolidCube(1.0);
     glPopMatrix();
-}
-
-void cube_draw(cube a, double z0, double y0, double x0) {
-    block_type (*array)[cube_size][cube_size] = cube_getArray(a);
-    
-    bool abled = cube_canPut(a, z0, y0, x0);
-    
-    for (int z = 0; z < cube_size; z++) {
-        for (int y = 0; y < cube_size; y++) {
-            for (int x = 0; x < cube_size; x++) {
-                drawCube(z0 + z, y0 + y, x0 + x, array[z][y][x], (abled ? 0.1 : 0.3));
-            }
-        }
-    }
-}
-
-void myReshape(int width, int height) {
-    // 투영 행렬 설정
-    glViewport(0, 0, width, height);
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    gluPerspective(40.0, (double)width / (double)height, 0.1, 100.0);
-    
-    screen_width = width;
-    screen_height = height;
-    
-    glutPostRedisplay();
-}
-
-int test_variable = 0; // test code
-int tv2 = 0;
-int tv3 = 0;
-
-void myDisplay(void) {
-    glClearColor(0.5, 0.5, 0.5, 1.0); // 배경색 지정
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
-    
-    // 카메라 설정
-    gluLookAt(-camera_x, camera_z, camera_y,               // 카메라 위치
-              -board_width / 2.0, 0.0, board_height / 2.0, // 바라볼 좌표: 보드의 가운데를 바라보도록 설정
-              0.0, 1.0, 0.0);                              // 위쪽을 가리키는 벡터
-    
-    // 게임판 그리기
-    for (int z = 0; z < max_height; z++) {
-        for (int y = 0; y < board_height; y++) {
-            for (int x = 0; x < board_width; x++) {
-                drawCube(z, y, x, board[z][y][x], 0.0);
-            }
-        }
-    }
-    
-    // test code
-    cube t = { 8, 0, 0, 0, false };
-    cube_draw(t, test_variable, tv2, tv3);
-    // test code
-    
-    glutSwapBuffers();
-}
-
-void myKeyboard(unsigned char key, int x, int y) {
-    // wasd키로 카메라 위치 조절
-    // 임시 코드로 자연스럽이 않음
-    // 한글 키로 바뀌어 있으면 작동하지 않음
-    // TODO: 카메라가 자연스럽게 움직이도록 코드 변경
-    if (key == 'w' || key == 'W') {
-        camera_y += 1.0;
-    } else if (key == 's' || key == 'S') {
-        camera_y -= 1.0;
-    } else if (key == 'a' || key == 'A') {
-        camera_x -= 1.0;
-    } else if (key == 'd' || key == 'D') {
-        camera_x += 1.0;
-    }
-    // test code
-    else if (key == 'r' || key == 'R') {
-        test_variable++;
-    } else if (key == 't' || key == 'T') {
-        tv2++;
-    } else if (key == 'y' || key == 'Y') {
-        tv3++;
-    }
-    
-    glutPostRedisplay();
-}
-
-void mySpecialKey(int key, int x, int y) {
-    // 방향키로 카메라 위치 조절
-    // 임시 코드로 자연스럽이 않음
-    // TODO: 카메라가 자연스럽게 움직이도록 코드 변경
-    if (key == GLUT_KEY_UP) {
-        camera_y += 1.0;
-    } else if (key == GLUT_KEY_DOWN) {
-        camera_y -= 1.0;
-    } else if (key == GLUT_KEY_LEFT) {
-        camera_x -= 1.0;
-    } else if (key == GLUT_KEY_RIGHT) {
-        camera_x += 1.0;
-    }
-}
-
-int main(int argc, char * argv[]) {
-    // 보드 초기화
-    for (int z = 0; z < max_height; z++) {
-        for (int y = 0; y < board_height; y++) {
-            for (int x = 0; x < board_width; x++) {
-                if (z == 0) {
-                    board[z][y][x] = ((x + y) % 2 == 0 ? WHITE : BLACK);
-                } else {
-                    board[z][y][x] = EMPTY;
-                }
-            }
-        }
-    }
-    
-    // 회전된 큐브 데이터 초기화
-    for (int cube_type = 0; cube_type < cube_num; cube_type++) {
-        int fit_size = fit_cube_size[cube_type];
-        block_type array[cube_size][cube_size][cube_size];
-        memcpy(array, cube_data[cube_type], cube_size * cube_size * cube_size * sizeof(block_type));
-        
-        block_type temp[cube_size][cube_size][cube_size];
-        for (int x_rotation = 0; x_rotation < 4; x_rotation++) {
-            for (int y_rotation = 0; y_rotation < 4; y_rotation++) {
-                for (int z_rotation = 0; z_rotation < 4; z_rotation++) {
-                    memcpy(rotated_cube_data[cube_type][z_rotation * 16 + y_rotation * 4 + x_rotation], array, cube_size * cube_size * cube_size * sizeof(block_type));
-                    
-                    memcpy(temp, array, cube_size * cube_size * cube_size * sizeof(block_type));
-                    for (int x = 0; x < fit_size; x++) {
-                        for (int y = 0; y < fit_size; y++) {
-                            for (int z = 0; z < fit_size; z++) {
-                                array[z][y][x] = temp[z][fit_size - 1 - x][y];
-                            }
-                        }
-                    }
-                }
-                memcpy(temp, array, cube_size * cube_size * cube_size * sizeof(block_type));
-                for (int x = 0; x < fit_size; x++) {
-                    for (int y = 0; y < fit_size; y++) {
-                        for (int z = 0; z < fit_size; z++) {
-                            array[z][y][x] = temp[fit_size - 1 - x][y][z];
-                        }
-                    }
-                }
-            }
-            memcpy(temp, array, cube_size * cube_size * cube_size * sizeof(block_type));
-            for (int x = 0; x < fit_size; x++) {
-                for (int y = 0; y < fit_size; y++) {
-                    for (int z = 0; z < fit_size; z++) {
-                        array[z][y][x] = temp[fit_size - 1 - y][z][x];
-                    }
-                }
-            }
-        }
-    }
-    
-    // 윈도우 생성과 초기화
-    glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
-    glutCreateWindow("BLOCKUS");
-    glutReshapeWindow(screen_width, screen_height);
-    
-    glEnable(GL_DEPTH_TEST);
-    
-    // 콜백 설정
-    glutDisplayFunc(myDisplay);
-    glutReshapeFunc(myReshape);
-    glutKeyboardFunc(myKeyboard);
-    
-    glutMainLoop();
-    
-    return 0;
 }
